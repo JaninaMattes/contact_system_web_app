d0df4c80249a227d652cd1fb2e4e83925f29f673
Katalin <kw062@hdm-stuttgart.de>
2018-05-05 21:50:19 +0200

    created class "Contact"

---
+++ src/de/hdm/kontaktsystem/shared/bo/Contact.java
@@ -0,0 +1,8 @@
+package de.hdm.kontaktsystem.shared.bo;
+
+/*Die Klasse Contact stellt die Kontakte dar, die von einem User erstellt werden bzw. auf welche ein User zugreifen kann, 
+sowie den User selbst.*/
+
+public class Contact extends BusinessObject {
+
+}
