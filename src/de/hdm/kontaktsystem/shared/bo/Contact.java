983da15a9f1673981f79f4808411cf4e444a078f
Katalin <kw062@hdm-stuttgart.de>
2018-05-07 15:46:43 +0200

    Klasse Contact -Kommentare bearbeitet -Konstruktor

--- src/de/hdm/kontaktsystem/shared/bo/Contact.java
+++ src/de/hdm/kontaktsystem/shared/bo/Contact.java
@@ -2,64 +2,97 @@ package de.hdm.kontaktsystem.shared.bo;
 
 import java.util.Date;
 
-/*Die Klasse Contact stellt die Kontakte dar, die von einem User erstellt werden bzw. auf welche ein User zugreifen kann, 
-sowie den User selbst.*/
+/**
+ * Die Klasse Contact stellt die Kontakte dar, die von einem User erstellt werden bzw. auf welche ein User zugreifen kann, 
+sowie den User selbst.
+ * @author Katalin
+ *
+ */
 
 
 public class Contact extends BusinessObject {
 
-	// Bezug zu dem User der den Kontakt besitzt
+	/**
+	 *  Bezug zu dem User der den Kontakt besitzt
+	 */
 	private User owner = null;
 	
-	//
+	/**
+	 * 
+	 */
 	private String status = null;
 	
-	//Name des Kontakts??
-	private String name = null;
+	/**
+	 * Name des Kontakts
+	 */
+	private PropertyValue name = null;
 	
-	//Datum wird von BusinessObject geerbt
+	/**
+	*Datum wird von BusinessObject geerbt
+	*/
 	
-	//Konstruktor
-	
-	public Contact(String name, String status, User owner) {
+	/**
+	 * Kontruktoren
+	 */
+
+	public Contact(PropertyValue name, String status, User owner) {
 		this.name = name;
 		this.status = status;
 		this.owner = owner;
 	}
 	
-	//Getter und Setter
+	public Contact(PropertyValue name, String status) {
+		this.name = name;
+		this.status = status;
+	}
 	
-	//Auslesen des Eigentümers
+	/**
+	 * Getter und Setter
+	 */
+	
+	/**
+	 * Auslesen des Eigentümers
+	 */
 	
 	public User getOwner() {
 		return owner;
 	}
 
-	//Eigentümer setzen
+	/**
+	 * Eigentümer setzen
+	 */
 
 	public void setOwner(User owner) {
 		this.owner = owner;
 	}
-	//Status auslesen
+	/**
+	 * Status auslesen
+	 */
 	
 	public String getStatus() {
 		return status;
 	}
 
-	//Status setzen
+	/**
+	 * Status setzen
+	 */
 
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	//Name auslesen
+	/**
+	 * Namen auslesen
+	 */
 	
-	public String getName() {
+	public PropertyValue getName() {
 		return name;
 	}
 
-	//Name setzen
+	/**
+	 * Namen setzen
+	 */
 
-	public void setName(String name) {
+	public void setName(PropertyValue name) {
 		this.name = name;
 	}
 	
