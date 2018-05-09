cb5ae4e26df9ab64e906e091ae86259df4ab30fe
MarcoPracher <mp127@hdm-stuttgart.de>
2018-05-09 17:10:31 +0200

    DBcon imp Update

--- src/de/hdm/kontaktsystem/server/db/DBConnection.java
+++ src/de/hdm/kontaktsystem/server/db/DBConnection.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.google.appengine.api.rdbms.AppEngineDriver;
+
 public class DBConnection {
 	
 	private static Connection con = null;
