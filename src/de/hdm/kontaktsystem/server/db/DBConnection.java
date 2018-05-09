f9dac3b6d5b2652da766b493c47a9c8d4a4a11df
OliverGorges <og016@hdm-stuttgart.de>
2018-05-09 22:11:41 +0200

    change URL from Test Database

--- src/de/hdm/kontaktsystem/server/db/DBConnection.java
+++ src/de/hdm/kontaktsystem/server/db/DBConnection.java
@@ -17,7 +17,8 @@ public class DBConnection {
 	//TODO: URL mit deren Hilfe die DB angesprochen wird
 		
 	// private static String url = ""; Google URL
-	private static String url = "jdbc:mysql://vweb12.nitrado.net/ni249339_1sql9?user=ni249339_1sql9&password=ITProjekt";
+	
+	private static String url = "jdbc:mysql://85.183.140.53:8170/ITProjekt?user=ITProjekt&password=ITProjekt";
 	
 	
 	public static Connection connection() {
