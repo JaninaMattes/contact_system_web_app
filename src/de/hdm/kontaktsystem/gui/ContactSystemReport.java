192d044fdd45834adc084142956403fe8970892f
SandraPrestel <sp074@hdm-stuttgart.de>
2018-05-16 18:21:43 +0200

    Create empty Classes ContactSystem, ContactSystemReport for client.gui, Created Interface ReportGenerator in Package shared

---
+++ src/de/hdm/kontaktsystem/gui/ContactSystemReport.java
@@ -0,0 +1,16 @@
+package de.hdm.kontaktsystem.gui;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+*
+*/
+public class ContactSystemReport implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/de/hdm/kontaktsystem/shared/ReportGenerator.java
@@ -0,0 +1,7 @@
+package de.hdm.kontaktsystem.shared;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+public interface ReportGenerator extends RemoteService {
+
+}
