e36cbda4ec9910970ec5cfd2d10c1d9ac362b7c3
MarcoPracher <mp127@hdm-stuttgart.de>
2018-05-17 13:56:37 +0200

    DBTestContactList Klasse Update

--- src/test/DBTestContactList.java
+++ src/test/DBTestContactList.java
@@ -39,7 +39,7 @@ public class DBTestContactList {
 		
 		ContactList cl = new ContactList();
 		cl.setName("Kontaktliste 1");
-		cl.setOwner(uMapper.getUserById(615));
+		cl.setOwner(uMapper.getUserById(01));
 		
 		Vector<ContactList> cll = new Vector<ContactList>();
 		
