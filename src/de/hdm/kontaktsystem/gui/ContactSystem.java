3b479702157a32a3240be5cfd3f35e57a26ee364
Katalin <kw062@hdm-stuttgart.de>
2018-05-20 17:03:23 +0200

    ContactSystem GUI Grund-Panels + Widgets

--- src/de/hdm/kontaktsystem/gui/ContactSystem.java
+++ src/de/hdm/kontaktsystem/gui/ContactSystem.java
@@ -1,16 +1,96 @@
 package de.hdm.kontaktsystem.gui;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.cellview.client.CellBrowser;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HeaderPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  *
  */
 public class ContactSystem implements EntryPoint {
 
+	/**
+	 * Instantiieren der GWT Widgets und Panels
+	 */
+	
+	//Panels
+	
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private VerticalPanel addP = new VerticalPanel();
+	private HorizontalPanel Panel = new HorizontalPanel();
+	private HeaderPanel hp = new HeaderPanel();
+	
+	//Widgets
+	
+	private Button ContactButton = new Button("Kontakte");
+	private Button ContactListsButton = new Button("Kontaktlisten");
+	private Button SharedButton = new Button("Geteilt");
+	private Label label = new Label();
+	
+	
+	//Oder mit CellBrowser-Widget umsetzen?
+	//private CellBrowser cellB = new CellBrowser();
+	
 	@Override
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
 		
-	}
+		/**
+		 * Widgets/Panels anordnen
+		 */
+		
+		addP.add(ContactButton);
+		addP.add(ContactListsButton);
+		addP.add(SharedButton);
+		Panel.add(addP);
+		
+		
+		//Panels und Widgets zum Hauptpanel hinzufügen
+		mainPanel.add(hp);
+		mainPanel.add(Panel);
+		mainPanel.add(label);
+		
+		//Verbindung des Hauptpanels mit dem Rootpanel TODO:Html-Dok.
+		RootPanel.get("HTMLPage").add(mainPanel);	
+	
+		
+		
+		//Text für Label setzen
+		label.setText("Label");
+		
+		
+		//ClickHandler für ContactButton
+		ContactButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+			//TODO: def. was passieren soll wenn man auf den Button klickt	
+			}
+		});
+		
+		//Clickhandler für ContactListButton
+		ContactListsButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+			//TODO: def. was passieren soll wenn man auf den Button klickt	
+			
+			}
+		});
+		
+		//Clickhandler für SharedButton
+		SharedButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+			//TODO: def. was passieren soll wenn man auf den Button klickt	
+			
+			}
+		});
+	
+	
+	
 
+}
 }
