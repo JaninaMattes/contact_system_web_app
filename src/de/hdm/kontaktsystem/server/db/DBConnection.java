d7ddf5d1c810eeca1507fb42b5759393d56f9c98
OliverGorges <og016@hdm-stuttgart.de>
2018-05-21 19:51:05 +0200

    Änderungen von Login zum Test

--- src/de/hdm/kontaktsystem/server/db/DBConnection.java
+++ src/de/hdm/kontaktsystem/server/db/DBConnection.java
@@ -4,35 +4,45 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import com.google.appengine.api.rdbms.AppEngineDriver;
-;
+import com.google.appengine.api.utils.SystemProperty;
+
 
 public class DBConnection {
 	
 	
 	private static Connection con = null;
 	
-	// Auswahl der verwendeten Datenbank
-	//private static String databaseTyp = "google";
-	private static String databaseTyp = "mySQL";
-	
 	
 	//TODO: URL mit deren Hilfe die DB angesprochen wird
-	// private static String url = " " Google URL
-	//private static String url = "jdbc:mysql://localhost:3306/it_projekt?user=root&password="; LocalHost
-	 private static String url = "jdbc:mysql://85.183.140.53:8170/ITProjekt?user=ITProjekt&password=ITProjekt"; 
+	private static String googleUrl = "jdbc:google:mysql://35.187.96.42:3306/ITProjekt?user=root&password=n6obMwAe51M9lnrxT9"; // Google URL
+	//private static String url = "jdbc:mysql://google/ITProjekt?cloudSqlInstance=ITProjekt&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=root&password=n6obMwAe51M9lnrxT9&useSSL=false"; //LocalHost
+	private static String localUrl = "jdbc:mysql://85.183.140.53:8170/ITProjekt?user=ITProjekt&password=ITProjekt"; 
 	
 	
 	
 	public static Connection connection() {
 		// falls noch keine Verbindung zur DB hergestellt wurde
 		if ( con == null ) {
+			String url = null;
 			try {
 				// Laden des Treibers
-				if(databaseTyp == "google"){
-					DriverManager.registerDriver(new AppEngineDriver());
-				}else{
-					Class.forName("com.mysql.jdbc.Driver");
-				}
+				DriverManager.registerDriver(new AppEngineDriver());
+				System.out.println(SystemProperty.environment.value());
+				if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+					
+					System.out.println("Load Google Driver");
+                    // Load the class that provides the new
+                    // "jdbc:google:mysql://" prefix.
+                    Class.forName("com.mysql.jdbc.GoogleDriver");
+                    url = googleUrl;
+                } else {
+                	System.out.println("Load MySQL Driver");
+                    // Local MySQL instance to use during development.
+                	Class.forName("com.mysql.jdbc.Driver");
+                    url = localUrl;
+                }
+				
+				
 				// Treiber Manager baut Verbindung mit Informationen aus url auf
 				con = DriverManager.getConnection(url);
 			} 
