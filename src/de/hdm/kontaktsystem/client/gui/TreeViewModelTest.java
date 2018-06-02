5c4c7c960a4908b9123ce922f0ed04b625b9960f
MarcoPracher <mp127@hdm-stuttgart.de>
2018-06-02 15:06:16 +0200

    TVMTest Klasse UPDATE

--- src/de/hdm/kontaktsystem/client/gui/TreeViewModelTest.java
+++ src/de/hdm/kontaktsystem/client/gui/TreeViewModelTest.java
@@ -12,6 +12,8 @@ import com.google.gwt.view.client.TreeViewModel;
 
 import de.hdm.kontaktsystem.client.ClientsideSettings;
 import de.hdm.kontaktsystem.shared.ContactSystemAdministrationAsync;
+import de.hdm.kontaktsystem.shared.bo.BusinessObject;
+import de.hdm.kontaktsystem.shared.bo.Contact;
 import de.hdm.kontaktsystem.shared.bo.ContactList;
 
 public class TreeViewModelTest implements TreeViewModel {
@@ -25,8 +27,10 @@ public class TreeViewModelTest implements TreeViewModel {
 	@Override
 	public <T> NodeInfo<?> getNodeInfo(T value) {
 		
+		//((ContactList) value).getContacts()
+		
 		//log("NodeInfo: "+ value);
-		ListDataProvider<ContactList> dataProvider = new ListDataProvider<ContactList>();
+		ListDataProvider<BusinessObject> dataProvider = new ListDataProvider<BusinessObject>();
 		
 		csa.getAllContactLists(new AsyncCallback<Vector<ContactList>>(){
 
@@ -51,19 +55,28 @@ public class TreeViewModelTest implements TreeViewModel {
 		//Anzahl der Elemente
 		for (int i = 0; i < 2; i++) {
 			ContactList cl = new ContactList();
+			Vector<Contact> cv = new Vector<Contact>();
+			for(int j = 0; j < 5; j++) {
+				Contact c = new Contact();
+				c.setBo_Id(j);
+				cv.add(c);
+			}
+			
+			cl.setContacts(cv);
 			cl.setName("Liste " + i);
-
+			cl.setBo_Id(i);
+			
 			dataProvider.getList().add(cl);
 			log(cl.getName()); 
 		}
-		return new DefaultNodeInfo<ContactList>(dataProvider, new DataCell());
+		return new DefaultNodeInfo<BusinessObject>(dataProvider, new DataCell());
 	}
 
 	@Override
 	public boolean isLeaf(Object value) {
 		log(value.toString());
 		// Anzahl der Ebenen
-		return value.toString().length() > 8;
+		return (value instanceof Contact ); //value.toString().length() > 8;
 	}
 
 	
@@ -76,11 +89,11 @@ public class TreeViewModelTest implements TreeViewModel {
 }
 
 
-class DataCell extends AbstractCell<ContactList>{
+class DataCell extends AbstractCell<BusinessObject>{
 
 	@Override
-	public void render(Context context, ContactList value, SafeHtmlBuilder sb) {
-		sb.appendHtmlConstant("<p>" + value.getName() + "</p>" );
+	public void render(Context context, BusinessObject value, SafeHtmlBuilder sb) {
+		sb.appendHtmlConstant("<p>" + value.getBoId() + "</p>" );
 		
 		
 	}
