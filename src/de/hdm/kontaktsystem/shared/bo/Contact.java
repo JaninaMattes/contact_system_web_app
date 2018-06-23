8770a6d77cf2960986471d5303c292c128cf20c2
SandraPrestel <sp074@hdm-stuttgart.de>
2018-06-23 18:49:27 +0200

    Kleinere Fehler in BO-Klassen beseitigt

--- src/de/hdm/kontaktsystem/shared/bo/Contact.java
+++ src/de/hdm/kontaktsystem/shared/bo/Contact.java
@@ -14,6 +14,11 @@ public class Contact extends BusinessObject {
 
 		
 	/**
+	 * Default Serial Version ID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
 	 * Eigenschaftsausprägungen des Kontakts
 	 */
 	
