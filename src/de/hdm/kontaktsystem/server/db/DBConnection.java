ec32439bb7105f95fc7f6d3830598f35456a531e
OliverGorges <og016@hdm-stuttgart.de>
2018-07-05 17:27:30 +0200

    add comments to DBConnetion & TreeViewModel

--- src/de/hdm/kontaktsystem/server/db/DBConnection.java
+++ src/de/hdm/kontaktsystem/server/db/DBConnection.java
@@ -6,39 +6,81 @@ import java.sql.SQLException;
 import com.google.appengine.api.rdbms.AppEngineDriver;
 import com.google.appengine.api.utils.SystemProperty;
 
+/**
+ * Verwalten einer Verbindung zur Datenbank.
+ * Aufbau der Klasse nach dem Singleton Pattern.
+ * Dadurch wird bei jedem Datenbankaufruf die selbe Datenbankverbindung verwendet.
+ * In der Klasse wird jeh nach Laufzeitumgebung(Lokal, Server) die dafür 
+ * vorgesehene Datenbank verwendet.  
+ * Auf die Goolge Cloud SQL Datenbank kann local nicht zugegriffen werden.
+ * 
+ * @author Oliver Gorges
+ *
+ */
 
 public class DBConnection {
 	
-	
+	 /**
+     * Die Klasse DBConnection wird nur einmal instantiiert. 
+     * Durch den Bezeichner <code>static</code> gilt diese Variable 
+     * global für alle Instanzen der DBConnection. 
+     * Die Variable speichert dabei die einzige Instanz dieser Klasse.
+     */
 	private static Connection con = null;
 	
 	
-	//TODO: URL mit deren Hilfe die DB angesprochen wird
-	//private static String googleUrl = "jdbc:google:mysql://35.187.96.42:3306/ITProjekt?user=root&password=n6obMwAe51M9lnrxT9"; // Google URL
-	private static String googleUrl =   "jdbc:google:mysql://itprojektss18-t9:europe-west3:itprojektss18-t9/ITProjekt?user=test&password=test";
-	//private static String url = "jdbc:mysql://google/ITProjekt?cloudSqlInstance=ITProjekt&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=root&password=n6obMwAe51M9lnrxT9&useSSL=false"; //LocalHost
-	private static String localUrl = "jdbc:mysql://85.183.140.53:8170/ITProjekt?user=ITProjekt&password=ITProjekt"; 
+	// URL zur verbindung mit der Goolge und der lokalen Datenbank
+	// Logindaten sollten im normalfall in ein Konfigurationsdokument ausgelagert werden.
+	private static String googleUrl = "jdbc:google:mysql://itprojektss18-t9:europe-west3:itprojektss18-t9/ITProjekt?user=test&password=test";
+	private static String localUrl =  "jdbc:mysql://127.0.0.1/ITProjekt?user=ITProjekt&password=ITProjekt"; 
 
 	
-	
+	/**
+     * Diese statische Methode kann aufgrufen werden durch
+     * <code>DBConnection.connection()</code>. Sie stellt die
+     * Singleton-Eigenschaft sicher, indem Sie dafuer sorgt, dass nur eine einzige
+     * Instanz von <code>DBConnection</code> existiert.<p>
+     *
+     * <b>Fazit:</b> DBConnection sollte nicht mittels <code>new</code>
+     * instantiiert werden, sondern stets durch Aufruf dieser statischen Methode.<p>
+     *
+     * <b>Nachteil:</b> Bei Zusammenbruch der Verbindung zur Datenbank - dies kann
+     * z.B. durch ein unbeabsichtigtes Herunterfahren der Datenbank ausgeloest
+     * werden - wird keine neue Verbindung aufgebaut, so dass die in einem solchen
+     * Fall die gesamte Software neu zu starten ist. In einer robusten Loesung
+     * wuerde man hier die Klasse dahingehend modifizieren, dass bei einer nicht
+     * mehr funktionsfuehigen Verbindung stets versucht wuerde eine neue Verbindung
+     * aufzubauen. Dies wuerde allerdings ebenfalls den Rahmen dieses Projekts
+     * sprengen.
+     *
+     * @return gültiges <code>DBConncetion</code>-Objekt.
+     * @see con
+     */
 	public static Connection connection() {
 		try {
-		// falls noch keine Verbindung zur DB hergestellt wurde oder nicht mehr gültig ist wird eine neue Verbindung aufgebaut.
+		/**
+		 *  Wenn noch keine Verbindung zur DB hergestellt wurde oder 
+		 *  die aktuelle Verbindung nicht mehr gültig ist wird eine neue Verbindung aufgebaut.
+		 */
 			if ( con == null || !con.isValid(2000) ) {
 				String url = null;
 				
 					// Laden des Treibers
 					DriverManager.registerDriver(new AppEngineDriver());
+					
+					/**
+					 * Überprüft ob das Projekt lokal in der Appengine läuft, oder ob das Projekt auf dem Google Server 
+					 * läuft.
+					 * Auf dem Google Server wird die Google Cloud SQL Datenbank verwendet.
+					 * Bei localen Tests wird eine locale Datenbank mit Testdaten verwendet.
+					 */
 					if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
 						
-						System.out.println("Load Google Driver");
-	                    // Load the class that provides the new
-	                    // "jdbc:google:mysql://" prefix.
+						 // Datenbank Treiber der zur Verbindung mit der lokalen Datenbank venötigt wird
 	                    Class.forName("com.mysql.jdbc.GoogleDriver");
 	                    url = googleUrl;
 	                } else {
-	                	//System.out.println("Load MySQL Driver");
-	                    // Local MySQL instance to use during development.
+	                    // Datenbank Treiber der zur Verbindung mit der lokalen Datenbank venötigt wird
 	                	Class.forName("com.mysql.jdbc.Driver");
 	                    url = localUrl;
 	                }
@@ -53,8 +95,7 @@ public class DBConnection {
 		}catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} 
-		
-		
+		// Rückgabe einer laufenden Datenbank verbindung
 		return con;
 	}
 
